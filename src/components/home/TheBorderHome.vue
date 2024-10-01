039efe331c2ca0f2995b18a3baf8bf51e48ea5e1
Benjamin.M <benjmonte@outlook.fr>
2024-10-01 10:42:17 +0200

    update base colors

--- src/components/home/TheBorderHome.vue
+++ src/components/home/TheBorderHome.vue
@@ -1,5 +1,20 @@
 <script setup></script>
 
 <template>
-  <div class="border home">TheBorderHome</div>
+  <div id="bordersite" class="home"></div>
 </template>
+
+<style scoped>
+#bordersite {
+  display: block;
+  position: fixed;
+  cursor: none;
+  pointer-events: none;
+  height: 100vh;
+  width: 100vw;
+  border: 20px var(--color-main) solid;
+  border-radius: 20px;
+  top: 0;
+  left: 0;
+}
+</style>
