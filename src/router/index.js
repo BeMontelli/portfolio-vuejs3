940970a2db8def0fbe4dd608c99906809daf95c1
Benjamin.M <benjmonte@outlook.fr>
2024-10-16 10:48:56 +0200

    Added privacy policy page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PrivacyPolicy from '../views/PrivacyPolicy.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,8 +8,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
-      meta: { bodyClass: 'bg-light' }
+      component: HomeView
+    },
+    {
+      path: '/politique-de-confidentialite',
+      name: 'privacy-policy',
+      component: PrivacyPolicy
     }
   ]
 })
---
+++ src/views/PrivacyPolicy.vue
@@ -0,0 +1,34 @@
+<script setup>
+import HeaderGeneral from '../components/general/HeaderGeneral.vue'
+import LoaderSite from '../components/LoaderSite.vue'
+import FooterGeneral from '../components/general/FooterGeneral.vue'
+import SectionTexts from '../components/general/SectionTexts.vue'
+</script>
+
+<template>
+  <LoaderSite />
+  <div class="app__container">
+    <div class="app__content">
+      <HeaderGeneral />
+      <main><SectionTexts /></main>
+      <FooterGeneral />
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<style scoped>
+.app__container {
+  background-color: var(--color-main);
+  padding: 15px;
+}
+.app__content {
+  background-color: var(--color-background);
+  border-radius: 15px;
+}
+body .app__content {
+  height: auto;
+  overflow: initial;
+}
+</style>
