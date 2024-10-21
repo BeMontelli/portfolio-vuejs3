06ba802c1da444ad0be502470f628878ac444aab
Benjamin.M <benjmonte@outlook.fr>
2024-10-21 12:35:39 +0200

    valid svg wave animation + display

--- src/components/home/SkillsHome.vue
+++ src/components/home/SkillsHome.vue
@@ -49,74 +49,86 @@ const skills = [
           </li>
         </ul>
       </div>
-
-      <svg
-        id="Calque_1"
-        data-name="Calque 1"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 754.11 110"
-      >
-        <path
-          d="M55.34,136.73A327.4,327.4,0,0,1,184.91,60c24.39-7.59,50.19-12.32,75.48-8.76,49.65,7,88.87,43.9,132.54,68.54,15,8.49,31.34,15.73,48.59,16.43,21.34.86,41.66-8.29,61.05-17.25L580,83.16C613.64,67.59,649,51.67,686.08,53.54c46.46,2.33,86.49,31.9,123.36,60.25"
-          transform="translate(-55.34 -35)"
-          id="number-two"
-          style="fill: none"
-        />
-        <text width="100%">
-          <textPath
-            alignment-baseline="center"
-            xlink:href="#number-two"
-            id="text-path"
-            text-anchor="start"
-            startOffset="0"
-          >
-            &nbsp;• Compétences1 • Compétences2 • Compétences3 • Compétences4 • Compétences5 •
-            Compétences6
-            <animate
-              attributeName="startOffset"
-              from="0%"
-              to="100%"
-              begin="0s"
-              dur="20s"
-              repeatCount="indefinite"
-            />
-          </textPath>
-          <textPath
-            alignment-baseline="center"
-            xlink:href="#number-two"
-            id="text-path-bis"
-            text-anchor="end"
-            startOffset="0"
-          >
-            • Compétences1 • Compétences2 • Compétences3 • Compétences4 • Compétences5 •
-            Compétences6
-            <animate
-              attributeName="startOffset"
-              from="0%"
-              to="100%"
-              begin="0s"
-              dur="20s"
-              repeatCount="indefinite"
-            />
-          </textPath>
-        </text>
-      </svg>
     </div>
+    <svg
+      id="bg__txt__wave"
+      data-name="Calque 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 754.11 110"
+    >
+      <path
+        d="M55.34,136.73A327.4,327.4,0,0,1,184.91,60c24.39-7.59,50.19-12.32,75.48-8.76,49.65,7,88.87,43.9,132.54,68.54,15,8.49,31.34,15.73,48.59,16.43,21.34.86,41.66-8.29,61.05-17.25L580,83.16C613.64,67.59,649,51.67,686.08,53.54c46.46,2.33,86.49,31.9,123.36,60.25"
+        transform="translate(-55.34 -35)"
+        id="number-two"
+        style="fill: none"
+      />
+      <text width="100%">
+        <textPath
+          alignment-baseline="center"
+          xlink:href="#number-two"
+          id="text-path"
+          text-anchor="start"
+          startOffset="0"
+        >
+          &nbsp;• Compétences • Compétences • Compétences • Compétences • Compétences • Compétences
+          <animate
+            attributeName="startOffset"
+            from="0%"
+            to="100%"
+            begin="0s"
+            dur="20s"
+            repeatCount="indefinite"
+          />
+        </textPath>
+        <textPath
+          alignment-baseline="center"
+          xlink:href="#number-two"
+          id="text-path-bis"
+          text-anchor="end"
+          startOffset="0"
+        >
+          • Compétences • Compétences • Compétences • Compétences • Compétences • Compétences
+          <animate
+            attributeName="startOffset"
+            from="0%"
+            to="100%"
+            begin="0s"
+            dur="20s"
+            repeatCount="indefinite"
+          />
+        </textPath>
+      </text>
+    </svg>
   </div>
 </template>
 
 <style scoped lang="scss">
+#bg__txt__wave {
+  pointer-events: none;
+  position: absolute;
+  top: calc(50% + 60px);
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  z-index: 0;
+  opacity: 0.5;
+  width: 100%;
+  @include responsiveTo(xxl) {
+    width: 1450px;
+  }
+}
 #text-path {
   text-transform: uppercase;
   font-weight: bolder;
   font-family: var(--font-nunito);
-  fill: red;
+  fill: var(--color-text);
+  font-size: 15.25px;
 }
 #text-path-bis {
   text-transform: uppercase;
   font-weight: bolder;
   font-family: var(--font-nunito);
-  fill: orange;
+  fill: var(--color-text);
+  font-size: 15.25px;
 }
 #home__skills {
   position: relative;
@@ -130,12 +142,14 @@ const skills = [
   -webkit-mask: var(--mask);
   mask: var(--mask);
   h2 {
+    position: relative;
     text-align: center;
     font-weight: bolder;
     font-style: italic;
     font-size: 3rem;
     line-height: 3rem;
     margin-bottom: 50px;
+    z-index: 1;
     color: var(--color-main);
     @include responsiveTo(xl) {
       font-size: 2rem;
@@ -170,6 +184,7 @@ const skills = [
 
   .skills {
     position: relative;
+    z-index: 1;
     .skills__list {
       position: relative;
       list-style: none;
